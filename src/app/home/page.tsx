39f4f3f9f2bb1efad96e3253b94658e1264a3f50
wangzyer <15372000216@163.com>
2023-03-08 23:33:28 +0800

    - init

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <img
+      src="https://img.alicdn.com/imgextra/i1/O1CN01Z0UB9L1eiXQaLD6Dx_!!6000000003905-2-tps-600-600.png"
+      style={{
+        width: 100,
+        height: 100,
+      }}
+    />
+  );
+}
